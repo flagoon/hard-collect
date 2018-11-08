fb612baaca1054286c3abdb0da204d18294e1cad
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-11-08 22:56:06 +0100

    Small improvements after changes to tsconfig.

--- src/Legend/Candy/Candy.tsx
+++ src/Legend/Candy/Candy.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import * as React from "react";
 import styled from "styled-components";
-import Donut from "../icons/donut.svg";
 
 const Candy = () => {
   return <CandyIcon />;
 };
 
-const CandyIcon = styled.span`
-  width: 5px;
+const CandyIcon = styled.div`
   border: 1px solid red;
+  width: 1rem;
+  height: 1rem;
+  /* -webkit-mask-size: 24px 24px;
+  mask-size: 24px 24px;
+  -webkit-mask-image: url("../icons/donut.svg");
   mask-image: url("../icons/donut.svg");
+  mask-position: 24px 24px; */
 `;
 
 export default Candy;
