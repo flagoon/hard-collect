531be6029835792dd94aa5b0af35fcd86f74e482
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-11-11 00:58:28 +0100

    Remove svg creators.

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,6 @@ class App extends Component {
           <Board />
           <Legend />
         </MainContainer>
-        <IconFooter>
-          <div>
-            Icons made by{" "}
-            <a
-              href="https://www.flaticon.com/authors/pixel-buddha"
-              title="Pixel Buddha"
-            >
-              Pixel Buddha
-            </a>{" "}
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
-            </a>{" "}
-            is licensed by{" "}
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-              target="_blank"
-            >
-              CC 3.0 BY
-            </a>
-          </div>
-        </IconFooter>
       </React.Fragment>
     );
   }
